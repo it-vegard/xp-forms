1666609838cbb6ab0904a895f660fdf297c34052
Vegard Haugstvedt <it.vegard@gmail.com>
2018-01-14 22:52:41 +0100

    chore: add eslint-rule 'no-underscore-dangle'

--- src/main/resources/assets/js/admin/index.jsx
+++ src/main/resources/assets/js/admin/index.jsx
@@ -17,9 +17,7 @@ const store = createStore(
   compose(
     applyMiddleware(thunkMiddleware),
     applyMiddleware(routerMiddleware(history)),
-    /* eslint-disable no-underscore-dangle */
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    /* eslint-enable */
   ),
 );
 
